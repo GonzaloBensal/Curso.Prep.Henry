ef608efd8770a5721a1343e6b838ba02793e50d9
Gonzalo Julian <GonzaloJulian_fb@hotmail.com>
2021-01-25 15:11:26 -0300

    CP JSVI 100%

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -29,7 +29,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   let nuevo = [];
-  forEach(array, cb.push(array))
+  forEach(array, function(elemento){
+    nuevo.push(cb(elemento));
+  });
+  return nuevo
 }
 
 // No modificar nada debajo de esta línea
